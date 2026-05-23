ee49e85f21e09de0d0c075e5d0c8fa30fd98c4ae
205572024-boop <205572024@eniac.edu.br>
2026-05-23 15:31:31 +0000

    admin_desenvolvimento

--- backend_pdi/PDI/admin.py
+++ backend_pdi/PDI/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
-
-# Register your models here.
+from.models import PDI
+@admin.register(PDI)
+class PDIAdmin(admin.ModelAdmin):
+    list_display = ("id", "name", "description", "created_at", "updated_at")
+    search_fields = ("name", "description")
+    list_filter = ("created_at", "updated_at")
